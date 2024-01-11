1d0df51d137c1dc75a0b2ad5f749ee0a4f593263
Lurain Grangbelr <pcaso5@naver.com>
2024-01-11 21:35:45 +0900

    [KKY] 프로필 업데이트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -96,34 +96,20 @@ export default function Home() {
       <Container className="w-full max-w-screen-xl px-5">
         <h2 className="font-logo_font">intro</h2>
         <h3 className="font-bold text-lg">1년 차 웹 개발자 김규연입니다.</h3>
-        <p className="whitespace-pre-line">{`기존 회사에 퍼블리셔로 입사하여 프론트엔드 백엔드를 가리지 않고 도전, 바닐라 JS로 시작하여 풀스택 개발자로 퇴사하였습니다.
-        일주일에 3개 이상 PR을 올릴 만큼 성실히 일했고, 팀에서 프론트엔드를 80% 전담하였습니다.
-        URL 단위로 설계되어 복잡했던 기존 React.js 프로젝트를 Next.js 아토믹 디자인 시스템으로 개선하였습니다.
+        <p className="whitespace-pre-line">{`프론트엔드 개발자로서 사내 여러 프로젝트를 담당하여 진행하였습니다.
+기존 회사에 퍼블리셔로 입사하여 부족한 개념을 채우기 위해 학습을 병행하며 프론트엔드 개발자로서 역량을 강화하였습니다.
 
 `}</p>
-        <h3 className="font-bold text-lg">(주)샵팬픽 2022.04 ~ 2023.11</h3>
-        <p className="whitespace-pre-line">{`[ 1년 9개월 / 프론트엔드 개발자 ]
-[ Next.js / React.js / MobX / Typescript / Prisma.io / Tailwind.css ]
-- 추가 프로젝트 진행 시 홈페이지, 관리자 페이지 레거시 코드 충돌로 인한 리워크 (React.js -> Next.js)
-- 아토믹 디자인 시스템 도입으로 컴포넌트 재사용성 높임
-- 퍼블리셔로 입사하여 컴포넌트 제작, RestfulAPI 제작, DB쿼리 등 다양한 업무 수행 (Prisma.io)
-- 데이터 조회 시 타입 지원 및 데이터 캐싱을 위해 SWR hook 제작 및 개선
-- MySQL DB 조회하여 엑셀 파일로 제작 및 암호 적용 다운로드 기능 구현
-
-`}</p>
-        <h3 className="font-bold text-lg">
-          더 체계적이고 고도화 된 개발 문화에 함께하고 싶습니다.
-        </h3>
+        <h3 className="font-bold text-lg">코드 리뷰에 익숙합니다.</h3>
         <p className="whitespace-pre-line">{`기존에 회사에서 Jira / Github / Vercel 기반으로 함께 프로젝트를 진행했습니다.
-        Github PR 및 코드 리뷰에 익숙합니다.
         스스로 부족한 점을 보완하고, 타인의 피드백을 수용할 수 있습니다.
 
 `}</p>
         <h3 className="font-bold text-lg">개발자로서의 목표</h3>
-        <p className="whitespace-pre-line">{`[ 더 고도화 된 프론트엔드 ]
+        <p className="whitespace-pre-line">{`더 고도화 된 프론트엔드
  - 사용자 접근성에 대한 연구
  - ThreeJS / Animate 등 다양한 프론트엔드 기술에 도전
-[ 풀스택 개발자 ]
+풀스택 개발자
  - 모든 범위의 개발 역량에 약점이 없는 개발자
  - 전체 프로젝트의 맥락을 파악하기 위해 모든 개발 범위에서 기초적인 지식과 기술의 흐름을 이해
 `}</p>
